99fc51d421453ebb3a575c970072ec13553f6474
Administrador2 <daplicacionesmoviles1@gmail.com>
2023-10-23 17:56:18 -0500

    agregando botón

--- app/src/main/java/pe/edu/idat/appfomsandroid/ListaActivity.java
+++ app/src/main/java/pe/edu/idat/appfomsandroid/ListaActivity.java
@@ -2,14 +2,17 @@ package pe.edu.idat.appfomsandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
 import pe.edu.idat.appfomsandroid.databinding.ActivityListaBinding;
 
-public class ListaActivity extends AppCompatActivity {
+public class ListaActivity extends AppCompatActivity implements
+        View.OnClickListener{
 
     private ActivityListaBinding binding;
 
@@ -25,6 +28,13 @@ public class ListaActivity extends AppCompatActivity {
                 listaPersonas
         );
         binding.lvpersonas.setAdapter(arrayAdapter);
+        binding.btnregresarregistro.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(getApplicationContext(),
+                RegistroActivity.class));
     }
 }
